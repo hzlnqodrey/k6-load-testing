a53ae88f6fed5ee488bd55aecddf6472669da049
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-08-16 00:04:56 +0700

    adding not and volumes delete in powershell script

---
+++ volumes-wipeout.ps1
@@ -0,0 +1,7 @@
+$processes = Get-Process "*docker desktop*"
+if ($processes.Count -gt 0)
+{
+    $processes[0].Kill()
+    $processes[0].WaitForExit()
+}
+Start-Process "C:\Program Files\Docker\Docker\Docker Desktop.exe"
